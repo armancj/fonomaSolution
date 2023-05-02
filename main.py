d9875e3d5980099c766a3100dccf43e4690c9202
armancj <armancanjer@gmail.com>
2023-05-02 09:18:46 -0400

    adding function of process_orders

--- main.py
+++ main.py
@@ -11,3 +11,8 @@ async def root():
 @app.get("/hello/{name}", tags=["App"])
 async def say_hello(name: str):
     return {"message": f"Hello {name}"}
+
+
+@app.post("solution", tags=["App"])
+async def process_orders(orders, criterion):
+    pass
