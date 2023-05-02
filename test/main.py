7eeddbe1645d8b12ef0a81b9eb4581ff390b7fcf
armancj <armancanjer@gmail.com>
2023-05-02 16:43:04 -0400

    adding test

---
+++ test/main.py
@@ -0,0 +1,16 @@
+import unittest
+
+from fastapi.testclient import TestClient
+
+from .main import app
+
+client = TestClient(app)
+
+
+class MyTestCase(unittest.TestCase):
+    def test_something(self):
+        self.assertEqual(True, False)  # add assertion here
+
+
+if __name__ == '__main__':
+    unittest.main()
